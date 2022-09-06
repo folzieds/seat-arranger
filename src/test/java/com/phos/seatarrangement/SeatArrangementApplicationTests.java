426b2e946d3876927ccc43fe51de99238718e4b7
folzieds <folzieds@gmail.com>
2022-09-07 00:51:38 +0100

    project setup

---
+++ src/test/java/com/phos/seatarrangement/SeatArrangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phos.seatarrangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatArrangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
