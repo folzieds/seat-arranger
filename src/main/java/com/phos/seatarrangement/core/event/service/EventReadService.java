6d0495047aaab4b060d85405e46447b268a8b8bb
folzieds <folzieds@gmail.com>
2022-09-09 01:06:34 +0100

    Added read for event

--- src/main/java/com/phos/seatarrangement/core/event/service/EventReadService.java
+++ src/main/java/com/phos/seatarrangement/core/event/service/EventReadService.java
@@ -5,10 +5,11 @@ import com.phos.seatarrangement.core.event.data.EventData;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Map;
 
 public interface EventReadService {
 
-    ResponseEntity<EventData> getEvent(String requestId);
+    ResponseEntity<Map<String, Object>> getEvent(String requestId);
 
-    ResponseEntity<List<EventData>> getAllEvents();
+    ResponseEntity<Map<String,Object>> getAllEvents();
 }
--- src/main/java/com/phos/seatarrangement/core/event/service/EventReadServiceImpl.java
+++ src/main/java/com/phos/seatarrangement/core/event/service/EventReadServiceImpl.java
@@ -1,6 +1,8 @@
 package com.phos.seatarrangement.core.event.service;
 
 import com.phos.seatarrangement.core.event.data.EventData;
+import com.phos.seatarrangement.core.event.domain.Event;
+import com.phos.seatarrangement.core.event.exception.EventNotFoundException;
 import com.phos.seatarrangement.core.event.repository.EventRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class EventReadServiceImpl implements EventReadService{
@@ -21,13 +25,47 @@ public class EventReadServiceImpl implements EventReadService{
     }
 
     @Override
-    public ResponseEntity<EventData> getEvent(String requestId) {
-        logger.info("Fetching event with request id {}", requestId);
-        return null;
+    public ResponseEntity<Map<String, Object>> getEvent(String requestId) {
+        try{
+            logger.info("Fetching event with request id {}", requestId);
+            Event event = eventRepository.findByRequestId(requestId);
+
+            return ResponseEntity.ok()
+                    .body(Map.of("status", "success", "data", mapObjectToData(event)));
+        }catch (Exception ex){
+            logger.error("Event with request id {} was not found...", requestId);
+            throw new EventNotFoundException("error.msg.event.not.found",
+                    "The event was not found");
+        }
     }
 
     @Override
-    public ResponseEntity<List<EventData>> getAllEvents() {
-        return null;
+    public ResponseEntity<Map<String, Object>> getAllEvents() {
+        try{
+            logger.info("Fetching all events...");
+            List<Event> events = eventRepository.findAll();
+            List<EventData> dataList = events.stream()
+                    .map(this::mapObjectToData)
+                    .collect(Collectors.toList());
+
+            logger.info("Events successfully fetched");
+            return ResponseEntity.ok()
+                    .body(Map.of("status", "Success", "data", dataList));
+        }catch (Exception ex){
+            logger.error("An error occurred while fetching all events...");
+            throw new EventNotFoundException("","",ex);
+        }
     }
+
+    private EventData mapObjectToData(Event event){
+        EventData data = new EventData();
+
+        if(event != null){
+            data.setDate(event.getDate());
+            data.setName(event.getName());
+            data.setAddress(event.getAddress());
+        }
+        return data;
+    }
+
 }
