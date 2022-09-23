40e4b4411407797b58dc5682aeaa00cae5edbbb6
folzieds <folzieds@gmail.com>
2022-09-23 07:39:03 +0100

    refactored user exception

--- src/main/java/com/phos/seatarrangement/core/useradministration/exception/UserNotFoundException.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/exception/UserNotFoundException.java
@@ -3,11 +3,11 @@ package com.phos.seatarrangement.core.useradministration.exception;
 import com.phos.seatarrangement.core.exception.AbstractPlatformException;
 
 public class UserNotFoundException extends AbstractPlatformException {
-    protected UserNotFoundException(String defaultGlobalCode, String defaultUserMessage) {
+    public UserNotFoundException(String defaultGlobalCode, String defaultUserMessage) {
         super(defaultGlobalCode, defaultUserMessage);
     }
 
-    protected UserNotFoundException(Throwable cause, String defaultGlobalCode, String defaultUserMessage) {
+    public UserNotFoundException(Throwable cause, String defaultGlobalCode, String defaultUserMessage) {
         super(cause, defaultGlobalCode, defaultUserMessage);
     }
 }
--- src/main/java/com/phos/seatarrangement/core/useradministration/exception/UsernameAlreadyExistException.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/exception/UsernameAlreadyExistException.java
@@ -3,11 +3,11 @@ package com.phos.seatarrangement.core.useradministration.exception;
 import com.phos.seatarrangement.core.exception.AbstractPlatformException;
 
 public class UsernameAlreadyExistException extends AbstractPlatformException {
-    protected UsernameAlreadyExistException(String defaultGlobalCode, String defaultUserMessage) {
+    public UsernameAlreadyExistException(String defaultGlobalCode, String defaultUserMessage) {
         super(defaultGlobalCode, defaultUserMessage);
     }
 
-    protected UsernameAlreadyExistException(Throwable cause, String defaultGlobalCode, String defaultUserMessage) {
+    public UsernameAlreadyExistException(Throwable cause, String defaultGlobalCode, String defaultUserMessage) {
         super(cause, defaultGlobalCode, defaultUserMessage);
     }
 }
