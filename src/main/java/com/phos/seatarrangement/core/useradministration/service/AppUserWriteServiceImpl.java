dbf9125849771aec411283a15ce3a46fefa1a126
folzieds <folzieds@gmail.com>
2022-09-24 23:56:00 +0100

    Added implementation for wirte service for users

--- src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserWriteServiceImpl.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserWriteServiceImpl.java
@@ -1,7 +1,24 @@
 package com.phos.seatarrangement.core.useradministration.service;
 
+import com.phos.seatarrangement.core.useradministration.data.AppUserData;
+import com.phos.seatarrangement.core.useradministration.data.AppUserResponseData;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppUserWriteServiceImpl implements AppUserWriteService{
+    @Override
+    public ResponseEntity<AppUserResponseData> create(AppUserData data) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AppUserResponseData> update(AppUserData data) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<AppUserResponseData> delete(Long appUserId) {
+        return null;
+    }
 }
