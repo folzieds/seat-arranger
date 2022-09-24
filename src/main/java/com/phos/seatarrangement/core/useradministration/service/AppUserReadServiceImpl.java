6e1823b1e3f9191f8fc177cc13ffc0096d6c6fe2
folzieds <folzieds@gmail.com>
2022-09-24 23:53:17 +0100

    Added implementation for reading users

--- src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserReadServiceImpl.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserReadServiceImpl.java
@@ -1,7 +1,20 @@
 package com.phos.seatarrangement.core.useradministration.service;
 
+import com.phos.seatarrangement.core.useradministration.data.AppUserData;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class AppUserReadServiceImpl implements AppUserReadService{
+    @Override
+    public ResponseEntity<AppUserData> fetchUser(Long appUserId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Collection<AppUserData>> fetchAllUsers() {
+        return null;
+    }
 }
