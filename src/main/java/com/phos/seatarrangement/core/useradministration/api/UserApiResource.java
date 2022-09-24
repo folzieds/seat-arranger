cd7cc6724fbf22f1542c83b4891d463043d314ef
folzieds <folzieds@gmail.com>
2022-09-24 23:45:58 +0100

    Added endpoints for creating users and fetching users

--- src/main/java/com/phos/seatarrangement/core/useradministration/api/UserApiResource.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/api/UserApiResource.java
@@ -1,9 +1,33 @@
 package com.phos.seatarrangement.core.useradministration.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.phos.seatarrangement.core.useradministration.data.AppUserData;
+import com.phos.seatarrangement.core.useradministration.data.AppUserResponseData;
+import com.phos.seatarrangement.core.useradministration.service.AppUserReadService;
+import com.phos.seatarrangement.core.useradministration.service.AppUserWriteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserApiResource {
+
+    private final AppUserReadService appUserReadService;
+    private final AppUserWriteService appUserWriteService;
+
+    @Autowired
+    public UserApiResource(AppUserReadService appUserReadService, AppUserWriteService appUserWriteService) {
+        this.appUserReadService = appUserReadService;
+        this.appUserWriteService = appUserWriteService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<AppUserResponseData> create(){
+        return null;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AppUserData> fetchUser(@PathVariable("id")Long appUserId){
+
+    }
 }
