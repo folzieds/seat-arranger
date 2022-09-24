76749c9d0bcedf9d4b6b9fb8de4781220b8fab8b
folzieds <folzieds@gmail.com>
2022-09-24 23:52:25 +0100

    Added methods for the write service for users

--- src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserWriteService.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/service/AppUserWriteService.java
@@ -1,4 +1,14 @@
 package com.phos.seatarrangement.core.useradministration.service;
 
+import com.phos.seatarrangement.core.useradministration.data.AppUserData;
+import com.phos.seatarrangement.core.useradministration.data.AppUserResponseData;
+import org.springframework.http.ResponseEntity;
+
 public interface AppUserWriteService {
+
+    ResponseEntity<AppUserResponseData> create(AppUserData data);
+
+    ResponseEntity<AppUserResponseData> update(AppUserData data);
+
+    ResponseEntity<AppUserResponseData> delete(Long appUserId);
 }
