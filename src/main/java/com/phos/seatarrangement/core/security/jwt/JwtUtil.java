2ad0dc940d11f2f6efe855fcab479ac49eaacea8
folzieds <folzieds@gmail.com>
2022-09-25 01:51:28 +0100

    Added jwt generator

--- src/main/java/com/phos/seatarrangement/core/security/jwt/JwtUtil.java
+++ src/main/java/com/phos/seatarrangement/core/security/jwt/JwtUtil.java
@@ -1,7 +1,13 @@
 package com.phos.seatarrangement.core.security.jwt;
 
+import com.phos.seatarrangement.core.useradministration.domain.AppUser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
 
 @Component
 public class JwtUtil {
@@ -12,7 +18,13 @@ public class JwtUtil {
     @Value("${seats.jwt.key.duration}")
     private Long jwtDuration;
 
-    public String getAccessToken(){
-        return null;
+    public String generateAccessToken(AppUser appUser){
+        return Jwts.builder()
+                .setSubject("")
+                .setIssuer("")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtDuration))
+                .signWith(SignatureAlgorithm.ES512, secretKey)
+                .compact();
     }
 }
