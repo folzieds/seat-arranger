b7b4004e8b57119572e8c45582c1ea1da24cc7d8
folzieds <folzieds@gmail.com>
2022-09-26 23:21:18 +0100

    refactoring security to present standards

--- src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
+++ src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
@@ -1,4 +1,9 @@
 package com.phos.seatarrangement.core.security.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecurityConfiguration {
+
+
 }
--- src/main/java/com/phos/seatarrangement/core/useradministration/repository/UserRepository.java
+++ src/main/java/com/phos/seatarrangement/core/useradministration/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.phos.seatarrangement.core.useradministration.repository;
 import com.phos.seatarrangement.core.useradministration.domain.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
 }
