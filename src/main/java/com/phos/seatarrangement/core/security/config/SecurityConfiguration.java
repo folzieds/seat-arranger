e169d0bd8147a529525152780123d63371c2f4a8
folzieds <folzieds@gmail.com>
2022-10-07 20:09:07 +0100

    added filter chain

--- src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
+++ src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
@@ -2,13 +2,27 @@ package com.phos.seatarrangement.core.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
