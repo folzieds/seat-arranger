6a5d6f9eafa21d590b2cc7cc03b1d189453fadfb
folzieds <folzieds@gmail.com>
2022-10-12 00:00:20 +0100

    security changes

--- src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
+++ src/main/java/com/phos/seatarrangement/core/security/config/SecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.phos.seatarrangement.core.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,17 +19,13 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .passwordManagement((management) -> management
-                        .changePasswordPage("/update-password"));
-
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> {
+                    auth.mvcMatchers("/").permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
