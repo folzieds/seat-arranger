61d9451151d94bc73cc2321b9545d3b75fd05bf7
folzieds <folzieds@gmail.com>
2022-10-15 22:45:41 +0100

    added rsa properties

---
+++ src/main/java/com/phos/seatarrangement/core/security/config/RsaKeyProperties.java
@@ -0,0 +1,11 @@
+package com.phos.seatarrangement.core.security.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "rsa")
+public class RSAProperties {
+
+
+
+
+}
